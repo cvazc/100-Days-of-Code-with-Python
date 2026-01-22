ba5cc804e41ab4e9a9d1e076f6aab441b0fa8026
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2026-01-22 22:21:30 +0000

    Working with Variables in Python to Manage Data

---
+++ Day_1/task.py
@@ -0,0 +1,19 @@
+# CLASS EXERCISES
+#print("Hello world!")
+
+#print("Hello world!\nHello world!\nHello world!")
+
+#print("Hello" + " " + "Sebastian")
+
+#print("Hello " + input("What is your name?") + "!")
+
+#name = input("What is your name?")
+
+#print(len(input("What is your name?")))
+
+# DAY 1 PROJECT
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + pet)
