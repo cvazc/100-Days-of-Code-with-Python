efc6ef30b0da14b2ca885a168e28e3380ab1ea19
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2026-01-27 22:17:41 +0000

    Python Functions & Karel

---
+++ Day_6/exercise01.py
@@ -0,0 +1,23 @@
+# Reeborg's World
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# Solution:
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def move_robot_to_final():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for step in range(6):
+#     move_robot_to_final()
---
+++ Day_6/task.py
@@ -0,0 +1,10 @@
+# Function
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+# my_function()
+
+# ------------
+
