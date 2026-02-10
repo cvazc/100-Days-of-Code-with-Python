19f488d2138648a6b0b865586061df962612d0bf
Sebastian <contreras.sebastian.dev@gmail.com>
2026-02-09 23:52:07 -0600

    Debugging: How to Find and Fix Errors in your Code

---
+++ Day_13/learning.py
@@ -0,0 +1,13 @@
+# def my_function():
+#     for i in range(1, 20):
+#     for i in range(1, 21):
+#         if i == 20:
+#             print("You got it")
+
+# my_function()
+
+# -----------------
+
+from random import randint
+
+dice_images = [""]
